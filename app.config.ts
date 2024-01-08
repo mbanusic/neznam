2a76daf0209da8447c1cb9b210928762ce86bba9
Marko Banusic <mbanusic@gmail.com>
2024-01-08 22:06:13 +0100

    save meta

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Ne znam',
+    description: 'Small business offering some insights',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -13,26 +13,34 @@ export default defineAppConfig({
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'ne-znam' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: '',
+
+        text: 'Ne znam'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
+      twitter: 'mbanusic',
       instagram: 'atinuxt',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'ne-znam'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
