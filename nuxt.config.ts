a7649f0917dc35e2eddd1aa73562d70404ff4f02
Marko Banušić <mbanusic@gmail.com>
2024-01-11 09:45:26 +0100

    prerender sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,5 +21,10 @@ export default defineNuxtConfig({
       url: 'https://nezn.am',
       logo: 'https://nezn.am/logo.png'
     }
+  },
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml']
+    }
   }
 })
